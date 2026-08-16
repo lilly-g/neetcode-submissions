611b36e8be1ede2cd6d2da9c7897b36763ad8a86
Lilly Gao <159401717+lilly-g@users.noreply.github.com>
2026-08-16 12:59:41 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # Initialize array of size n
+        # memo[i] = # of solutions from step i
+        memo = [-1] * n
+
+        def climb(i) :
+            if i == n :
+                return 1
+            if i > n :
+                return 0
+
+            if memo[i] != -1 :
+                return memo[i]
+            
+            memo[i] = climb(i+1) + climb(i+2)
+            return memo[i]
+
+        return climb(0)
