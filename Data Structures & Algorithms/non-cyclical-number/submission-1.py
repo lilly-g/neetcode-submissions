e202170746093f8eb5ae6eae7c40c27eeecb19f5
Lilly Gao <159401717+lilly-g@users.noreply.github.com>
2026-08-19 21:16:46 -0400

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        cache = set()
+        
+        while n != 1 :
+            if n in cache :
+                return False
+                
+            cache.add(n)
+            temp = n
+            n = 0
+            for d in str(temp) :
+                print(d)
+                n += int(d) ** 2
+                print(n)
+        
+        return True
